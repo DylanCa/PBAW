a2f1bbcd08d50876910db51842855def889ebdfc
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 11:58:50 +0200

    Add: Item.py class - to make API calls to Item

---
+++ wow_api/wow_community/Item.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class Item():
+    def __init__(self, apikey=''):
+        self.apikey = apikey
+
+    def item(self, server="eu", itemID=18803,
+                         locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/item/{}?locale={}&apikey={}'.format(
+            server, itemID, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
+
+    def itemSet(self, server="eu", setID=1060,
+                         locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/item/set/{}?locale={}&apikey={}'.format(
+            server, setID, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
