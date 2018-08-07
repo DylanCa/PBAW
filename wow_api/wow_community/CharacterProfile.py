5d40bda57e43ec364ba298b4fbf66d02a17eb7e3
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 10:06:48 +0200

    Add: characterProfile() API call method to CharachterProfile.py

--- wow_api/wow_community/CharacterProfile.py
+++ wow_api/wow_community/CharacterProfile.py
@@ -1,3 +1,19 @@
+import requests
+
+
 class CharacterProfile():
     def __init__(self, apikey=''):
         self.apikey = apikey
+
+    def characterProfile(self,
+                         server="eu",
+                         realm="archimonde",
+                         characterName="Protòtype",
+                         locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/character/{}/{}?locale={}&apikey={}'.format(
+            server, realm, characterName, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
