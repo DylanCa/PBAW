4ac9f6db5929e199ea461e180235b38ef09de23b
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 10:12:52 +0200

    Add: progression() API call method to CharacterProfile.py

--- wow_api/wow_community/CharacterProfile.py
+++ wow_api/wow_community/CharacterProfile.py
@@ -147,3 +147,16 @@ class CharacterProfile():
         response = requests.get(url)
 
         return response.text
+
+    def progression(self,
+                    server="eu",
+                    realm="archimonde",
+                    characterName="Protòtype",
+                    locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/character/{}/{}?fields=progression&locale={}&apikey={}'.format(
+            server, realm, characterName, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
