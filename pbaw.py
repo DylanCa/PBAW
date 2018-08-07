da57fdff72e1a5cd941833b0bf2396642231abe6
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 17:01:57 +0200

    Add: Diablo3Community to the initialisation of Pbaw

--- pbaw.py
+++ pbaw.py
@@ -1,6 +1,8 @@
 from models.wow_community import WowCommunity
+from models.diablo3_community import Diablo3Community
 
 
 class Pbaw():
     def __init__(self, apikey=''):
         self.wowc = WowCommunity(apikey)
+        self.d3c = Diablo3Community(apikey)
