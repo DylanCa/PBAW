e40c51a4d5f7465573606650e1e6726be12a518e
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 10:14:57 +0200

    Add: talents() API call method to CharacterProfile.py

--- wow_api/wow_community/CharacterProfile.py
+++ wow_api/wow_community/CharacterProfile.py
@@ -225,3 +225,16 @@ class CharacterProfile():
         response = requests.get(url)
 
         return response.text
+
+    def talents(self,
+                server="eu",
+                realm="archimonde",
+                characterName="Protòtype",
+                locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/character/{}/{}?fields=talents&locale={}&apikey={}'.format(
+            server, realm, characterName, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
