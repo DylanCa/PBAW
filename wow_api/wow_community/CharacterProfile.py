7c4cd35b86a02c7636d724b0ef985ca5596032ab
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 10:08:55 +0200

    Add: hunterPets() API call method to CharachterProfile.py

--- wow_api/wow_community/CharacterProfile.py
+++ wow_api/wow_community/CharacterProfile.py
@@ -69,3 +69,16 @@ class CharacterProfile():
         response = requests.get(url)
 
         return response.text
+
+    def hunterPets(self,
+                   server="eu",
+                   realm="archimonde",
+                   characterName="Protòtype",
+                   locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/character/{}/{}?fields=hunterPets&locale={}&apikey={}'.format(
+            server, realm, characterName, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
