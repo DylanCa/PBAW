ffd8a9763f99a967770f8b24c2857acdbd65cab1
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 17:00:08 +0200

    Add: ArtisanAndRecipe.py class - to make API calls to ArtisanAndRecipe

---
+++ models/diablo3_community/ArtisanAndRecipe.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class ArtisanAndRecipe():
+    def __init__(self, apikey=''):
+        self.apikey = apikey
+
+    def getArtisan(self, server="eu", artisanSlug='blacksmith', locale="en_US"):
+
+        url = 'https://{}.api.battle.net/d3/data/artisan/{}?locale={}&apikey={}'.format(
+            server, artisanSlug, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
+
+    def getRecipe(self, server="eu", artisanSlug='blacksmith', recipeSlug='apprentice-flamberge', locale="en_US"):
+
+        url = 'https://{}.api.battle.net/d3/data/artisan/{}/recipe/{}?locale={}&apikey={}'.format(
+            server, artisanSlug, recipeSlug, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
