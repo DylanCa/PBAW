5489f7c6bf59968a67c05a1ef40ab8b09977da8a
Prototype <dylan.Cattelan@gmail.com>
2018-08-07 10:09:55 +0200

    Add: mounts() API call method to CharachterProfile.py

--- wow_api/wow_community/CharacterProfile.py
+++ wow_api/wow_community/CharacterProfile.py
@@ -95,3 +95,16 @@ class CharacterProfile():
         response = requests.get(url)
 
         return response.text
+
+    def mounts(self,
+               server="eu",
+               realm="archimonde",
+               characterName="Protòtype",
+               locale="en_US"):
+
+        url = 'https://{}.api.battle.net/wow/character/{}/{}?fields=mounts&locale={}&apikey={}'.format(
+            server, realm, characterName, locale, self.apikey)
+
+        response = requests.get(url)
+
+        return response.text
