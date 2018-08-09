8e4c652d1744fc822acfa27d44b1dd0cdadc724f
Prototype <dylan.Cattelan@gmail.com>
2018-08-09 10:21:09 +0200

    Add: Ladder and DataRessources to SC2 initialisation

--- models/sc2_community/__init__.py
+++ models/sc2_community/__init__.py
@@ -1,6 +1,10 @@
 from.Profile import Profile
+from .Ladder import Ladder
+from .DataRessources import DataRessources
 
 
 class Sc2Community():
     def __init__(self, apikey):
         self.profile = Profile(apikey)
+        self.ladder = Ladder(apikey)
+        self.data_ressources = DataRessources(apikey)
