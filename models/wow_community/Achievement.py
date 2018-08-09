63db178ad3392b374f1b6c1aed0e024dd1097c51
Prototype <dylan.Cattelan@gmail.com>
2018-08-09 12:33:09 +0200

    Pre-release refactor ( removed whitespaces + blank lines )

--- models/wow_community/Achievement.py
+++ models/wow_community/Achievement.py
@@ -2,7 +2,6 @@ from models import Fetcher
 
 
 class Achievement():
-    
     def achievement(self, server="eu", achievementID="2144", locale="en_US"):
 
         self.route = '/wow/achievement/{}'.format(achievementID)
--- models/wow_community/Auction.py
+++ models/wow_community/Auction.py
@@ -2,7 +2,6 @@ from models import Fetcher
 
 
 class Auction():
-
     def auction(self, server="eu", realm="archimonde", locale="en_US"):
 
         self.route = '/wow/auction/data/{}'.format(realm)
